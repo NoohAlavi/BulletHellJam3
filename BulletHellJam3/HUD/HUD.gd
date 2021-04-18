10a1bccf16f846b4a217b124efc51357604bcfab
Nooh Alavi <noohalavi@gmail.com>
2021-04-18 12:17:29 -0400

    Added Invincibility Label; Changed Grace Period Anim

--- BulletHellJam3/HUD/HUD.gd
+++ BulletHellJam3/HUD/HUD.gd
@@ -9,6 +9,10 @@ onready var debug_info = $DebugInfo/VBoxContainer
 onready var hud = $HUD
 
 func _process(delta: float) -> void:
+	update_hud()
+	update_debug()
+
+func update_debug():
 	debug_info.get_node("FPSLabel").text = "FPS: " + str(Engine.get_frames_per_second())
 	debug_info.get_node("MemUsage").text = "Memory Usage: " + str(OS.get_static_memory_usage()) + "B"
 	debug_info.get_node("WindowSize").text = "Window Size: " + str(OS.window_size)
@@ -16,10 +20,7 @@ func _process(delta: float) -> void:
 	debug_info.get_node("EnemyCountLabel").text = "Spawned Enemies: " + str(len(get_parent().get_parent().get_node("EnemyHolder").get_children()))
 	debug_info.get_node("PlayerHealthLabel").text = "Player Health: " + str(player.health) + " / " + str(player.max_health)
 	debug_info.get_node("NextSpawnTime").text = "Time Before Next Spawn: " + str(timer.time_left)
-	
-	hud.get_node("HealthLabel").text = "HP: " + str((player.health / player.max_health) * 100) + "%"
-	hud.get_node("ScoreLabel").text = "Score: " + str(player.score)
-	
+
 	if len(Input.get_connected_joypads()) > 0:
 		debug_info.get_node("IsControllerConnected").text = "Controller Connected: True"
 	else:
@@ -27,3 +28,10 @@ func _process(delta: float) -> void:
 	
 	if Input.is_action_just_pressed("toggle_debug"):
 		$DebugInfo.visible = !$DebugInfo.visible
+
+func update_hud():
+	hud.get_node("HealthLabel").text = "HP: " + str((player.health / player.max_health) * 100) + "%"
+	hud.get_node("ScoreLabel").text = "Score: " + str(player.score)
+	var is_invincible = "Yes" if player.is_invincible else "No"
+	hud.get_node("InvincibleLabel").text = "Invincible: " + str(is_invincible)
+	
