1a969b8c75b8594ccd178b7eba2aa6e041701c11
Nooh Alavi <noohalavi@gmail.com>
2021-04-20 15:28:05 -0400

    Added credits scene

--- BulletHellJam3/MainMenu/MainMenu.gd
+++ BulletHellJam3/MainMenu/MainMenu.gd
@@ -1,13 +1,18 @@
 extends Control
 
+export var target_scene = "res://World/World.tscn"
+
 func _on_PlayButton_pressed() -> void:
 	$AnimationPlayer.play("SceneTransition")
-
+	target_scene = "res://World/World.tscn"
 
 func _on_QuitButton_pressed() -> void:
 	get_tree().quit()
 
-
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
 	if anim_name == "SceneTransition":
-		get_tree().change_scene("res://World/World.tscn")
+		get_tree().change_scene(target_scene)
+
+func _on_ControlsButton_pressed() -> void:
+	$AnimationPlayer.play("SceneTransition")
+	target_scene = "res://Credits/Credits.tscn"
