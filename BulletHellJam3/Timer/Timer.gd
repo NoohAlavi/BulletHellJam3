d0a268751ecb60f54a8f4b3b21ed86704bc87e1a
Nooh Alavi <noohalavi@gmail.com>
2021-04-21 17:14:06 -0400

    Fixed timer

--- BulletHellJam3/Timer/Timer.gd
+++ BulletHellJam3/Timer/Timer.gd
@@ -1,13 +1,14 @@
 extends Sprite
 
 func set_time(time):
-	if time < 1:
+	print(time)
+	if time < 10:
 		texture = load("res://Timer/Timer_1.png")
-	if time < 2.5:
+	if time < 7.5:
 		texture = load("res://Timer/Timer_2.png")
 	if time < 5:
 		texture = load("res://Timer/Timer_3.png")
-	if time < 7.5:
+	if time < 2.5:
 		texture = load("res://Timer/Timer_4.png")
-	if time < 10:
+	if time < 0.5:
 		texture = load("res://Timer/Timer_5.png")
