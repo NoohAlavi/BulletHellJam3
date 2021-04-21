c3b9378b17cfacfd12fc23344a7d2e1f5cbccf9a
Nooh Alavi <noohalavi@gmail.com>
2021-04-21 14:43:49 -0400

    Replaced quit button with credits, fixed automatic play bug

--- BulletHellJam3/MainMenu/MainMenu.gd
+++ BulletHellJam3/MainMenu/MainMenu.gd
@@ -3,7 +3,7 @@ extends Control
 export var target_scene = "res://World/World.tscn"
 
 func _on_PlayButton_pressed() -> void:
-	$AnimationPlayer.play_backwards("SceneTransition")
+	$AnimationPlayer.play("SceneTransition")
 	target_scene = "res://World/World.tscn"
 
 func _on_QuitButton_pressed() -> void:
@@ -14,5 +14,9 @@ func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
 		get_tree().change_scene(target_scene)
 
 func _on_ControlsButton_pressed() -> void:
-	$AnimationPlayer.play_backwards("SceneTransition")
+	$AnimationPlayer.play("SceneTransition")
 	target_scene = "res://Controls/Controls.tscn"
+
+func _on_CreditsButton_pressed() -> void:
+	$AnimationPlayer.play("SceneTransition")
+	target_scene = "res://Credits/Credits.tscn"
